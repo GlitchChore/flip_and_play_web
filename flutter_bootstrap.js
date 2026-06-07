7340542b8f3d1d286c7d7ea0780d8212c9134185
gerlett <gerlett@gmx.at>
2026-06-07 15:07:10 +0200

    new build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2816964211" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2285397820" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
